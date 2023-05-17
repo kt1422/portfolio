3a9cce75b0e0ad5b1e0e893bf0d6c0612bb6bcd2
Kenneth Tan <kennethtan1414@gmail.com>
2023-05-17 21:24:16 +0800

    done

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/home', (req, res)=>{
-    res.render('home', {title:"Home"});
+    res.render('home', {title:"Kenneth Tan"});
 });
 
 module.exports = router;
